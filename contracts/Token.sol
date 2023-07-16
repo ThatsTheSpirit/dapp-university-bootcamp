84f7538fd53d16518f855c70d8b6eb7827eb3c9a
1999bag <1999bag@gmail.com>
2023-07-17 01:06:53 +0300

    Smoke test

---
+++ contracts/Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string private name;
+    string private symbol;
+
+    constructor() {
+        
+    }
+}
